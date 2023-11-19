7fdb6da6a55b11e652ab79240b2c5b4c89eac6d6
Ravi Shankar <ravishankarpl3@gmail.com>
2023-11-19 07:07:21 -0800

    "Initial"

--- src/main/java/com/example/demo/MedApplication.java
+++ src/main/java/com/example/demo/MedApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MedApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(MedApplication.class, args);
 	}
